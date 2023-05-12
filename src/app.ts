f54787bebd49269ead4ffd7ccbad0b729acc2976
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-05-12 15:52:23 +0600

    tldrew extention

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ const app: Application = express();
 // cors
 app.use(cors());
 
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
