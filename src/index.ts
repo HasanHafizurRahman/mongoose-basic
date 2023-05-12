9898d4c102959885580f56440723608761d131e0
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-05-12 12:47:41 +0600

    types integreted

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
 import mongoose from "mongoose";
-import express from "express";
-// const express = require("express");
-const app = express();
-const port = 5000;
+import app from "./app";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const port: number = 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-async function main() {
+async function connect() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/test");
     console.log("Database connection successfully");
   } catch (error) {
-    console.log(error);
+    console.log("Cannot connect database", error);
   }
 }
+
+connect();
